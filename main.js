38ec95576ff5a553660c9be390bdb6c1f0c00349
Kavya Shree <kavyamailme1001@gmail.com>
2021-03-30 18:57:29 +0530

    added menu

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu } = require("electron");
 
-process.env.NODE_ENV = 'development'
+process.env.NODE_ENV = "development";
 
-const isDev = process.env.NODE_ENV !== 'production' ? true : false
-const isMac = process.platform == 'darwin' ? true : false
+const isDev = process.env.NODE_ENV !== "production" ? true : false;
+const isMac = process.platform == "darwin" ? true : false;
 
 let mainWindow;
 
@@ -13,22 +13,45 @@ function createMainWindow() {
     height: 600,
     title: "Image Shrink",
     icon: `${__dirname}/assets/icons/Icon_256x256.png`,
-    resizable : isDev ? true : false,
+    resizable: isDev ? true : false,
   });
   // mainWindow.loadURL(`file://${__dirname}/app/index.html`)
   mainWindow.loadFile("./app/index.html");
 }
 
-app.on("ready", createMainWindow);
+app.on("ready", () => {
+  createMainWindow();
 
-app.on('window-all-closed', () => {
-    if (!isMac) {
-      app.quit()
-    }
-  })
-  
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createMainWindow()
-    }
-  })
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+
+  mainWindow.on("ready", () => (mainWindow = null));
+});
+
+const menu = [
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Quit",
+        click: () => app.quit(),
+      },
+    ],
+  },
+];
+
+if(isMac) {
+  menu.unshift({role: 'appMenu'})
+}
+
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
