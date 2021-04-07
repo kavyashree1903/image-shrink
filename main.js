f9a48abbc5d06924f424bed9f31572e7dae81d6b
Kavya Shree <65967490+kavyashree1903@users.noreply.github.com>
2021-04-07 15:31:44 +0530

    update index.html

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Menu } = require("electron");
+const { app, BrowserWindow, Menu, globalShortcut } = require("electron");
 
 process.env.NODE_ENV = "development";
 
@@ -6,6 +6,7 @@ const isDev = process.env.NODE_ENV !== "production" ? true : false;
 const isMac = process.platform == "darwin" ? true : false;
 
 let mainWindow;
+let aboutWindow;
 
 function createMainWindow() {
   mainWindow = new BrowserWindow({
@@ -14,9 +15,23 @@ function createMainWindow() {
     title: "Image Shrink",
     icon: `${__dirname}/assets/icons/Icon_256x256.png`,
     resizable: isDev ? true : false,
+    backgroundColor: "white",
   });
   // mainWindow.loadURL(`file://${__dirname}/app/index.html`)
   mainWindow.loadFile("./app/index.html");
+}
+
+function createAboutWindow() {
+  aboutWindow = new BrowserWindow({
+    width: 300,
+    height: 300,
+    title: "About Image Shrink",
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: false,
+    backgroundColor: "white",
+  });
+  // aboutWindow.loadURL(`file://${__dirname}/app/index.html`)
+  aboutWindow.loadFile("./app/about.html");
 }
 
 app.on("ready", () => {
@@ -25,23 +40,73 @@ app.on("ready", () => {
   const mainMenu = Menu.buildFromTemplate(menu);
   Menu.setApplicationMenu(mainMenu);
 
+  globalShortcut.register("CmdorCtrl+R", () => {
+    mainWindow.reload();
+  });
+
+  globalShortcut.register(isMac ? "Command+Alt+I" : "Ctrl+Shift+I", () => {
+    mainWindow.toggleDevTools();
+  });
+
   mainWindow.on("ready", () => (mainWindow = null));
 });
 
 const menu = [
-  {
-    label: "File",
-    submenu: [
-      {
-        label: "Quit",
-        click: () => app.quit(),
-      },
-    ],
-  },
+  ...(isMac
+    ? [
+        {
+          label: app.name,
+          submenu: [
+            {
+              label: "About",
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+  // {
+  //   label: "File",
+  //   submenu: [
+  //     {
+  //       label: "Quit",
+  //       // accelerator: isMac ? 'Command+W' : 'Ctrl+W',
+  //       accelerator : "CmdorCtrl+W",
+  //       click: () => app.quit(),
+  //     },
+  //   ],
+  // },
+  { role: "fileMenu" },
+  ...(!isMac
+    ? [
+        {
+          label: "Help",
+          submenu: [
+            {
+              label: "About",
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+  ...(isDev
+    ? [
+        {
+          label: "Developer",
+          submenu: [
+            { role: "reload" },
+            { role: "forcereload" },
+            { type: "separator" },
+            { role: "toggledevtools" },
+          ],
+        },
+      ]
+    : []),
 ];
 
-if(isMac) {
-  menu.unshift({role: 'appMenu'})
+if (isMac) {
+  menu.unshift({ role: "appMenu" });
 }
 
 app.on("window-all-closed", () => {
